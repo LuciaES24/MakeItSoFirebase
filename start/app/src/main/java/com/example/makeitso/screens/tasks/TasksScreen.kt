006dd888553bbfd0140ca2f5a46c98d1ec557b58
LuciaES24 <lespsan543@g.educaand.es>
2024-02-02 17:26:45 +0100

    terminado firebase

--- start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
+++ start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.makeitso.R.drawable as AppIcon
 import com.example.makeitso.R.string as AppText
 import com.example.makeitso.common.composable.ActionToolbar
@@ -47,7 +48,8 @@ fun TasksScreen(
     onSettingsClick = viewModel::onSettingsClick,
     onTaskCheckChange = viewModel::onTaskCheckChange,
     onTaskActionClick = viewModel::onTaskActionClick,
-    openScreen = openScreen
+    openScreen = openScreen,
+    viewModel = viewModel
   )
 
   LaunchedEffect(viewModel) { viewModel.loadTaskOptions() }
@@ -62,8 +64,14 @@ fun TasksScreenContent(
   onSettingsClick: ((String) -> Unit) -> Unit,
   onTaskCheckChange: (Task) -> Unit,
   onTaskActionClick: ((String) -> Unit, Task, String) -> Unit,
-  openScreen: (String) -> Unit
+  openScreen: (String) -> Unit,
+  viewModel: TasksViewModel = hiltViewModel()
 ) {
+  val tasks = viewModel
+    .tasks
+    .collectAsStateWithLifecycle(emptyList())
+  val options by viewModel.options
+
   Scaffold(
     floatingActionButton = {
       FloatingActionButton(
@@ -87,10 +95,10 @@ fun TasksScreenContent(
       Spacer(modifier = Modifier.smallSpacer())
 
       LazyColumn {
-        items(emptyList<Task>(), key = { it.id }) { taskItem ->
+        items(tasks.value, key = { it.id }) { taskItem ->
           TaskItem(
             task = taskItem,
-            options = listOf(),
+            options = options,
             onCheckChange = { onTaskCheckChange(taskItem) },
             onActionClick = { action -> onTaskActionClick(openScreen, taskItem, action) }
           )
